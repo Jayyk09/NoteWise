ff684b5635f8a443bfa76dd12b510261b176a176
jroy <jayyk0912@gmail.com>
2024-10-28 16:32:08 -0500

    initialize firebase storage

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 import time
-#secrets.toml key
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 st.title("Notewise")
 
@@ -9,6 +10,21 @@ st.title("Notewise")
 genai_api_key = st.secrets["GEMINI_API"]
 
 genai.configure(api_key=genai_api_key)
+
+# Firebase authentication
+
+cred = credentials.Certificate("firebase-credentials.json")
+
+@st.cache_resource
+def init_firebase():
+    cred = credentials.Certificate("firebase-credentials.json")
+    firebase_admin.initialize_app(cred)
+    return firestore.client()
+
+db = init_firebase()
+
+# Auth with just username for the sake of ease
+username = st.text_input("Username")
 
 def analyze_video(uploaded_file, display_name):
     """
@@ -31,7 +47,6 @@ def analyze_video(uploaded_file, display_name):
             print(f"Uploading file...")
             video_file = genai.upload_file(
                 path=uploaded_file, 
-  # Use video_bytes directly
                 display_name=display_name,
                 mime_type="video/mp4",
                 resumable=True
@@ -50,28 +65,29 @@ def analyze_video(uploaded_file, display_name):
             raise ValueError(video_file.state.name)
 
         # Generate content using the uploaded file
-        prompt = "Describe this video."
+        prompt = "write lecture for this video with specific time stamps. at the end of it, provide summary of the video for review"
         model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
         print("Making LLM inference request...")
         response = model.generate_content(
             [video_file, prompt], request_options={"timeout": 600} # Increase timeout to 10 minutes 
         )
         return response.text
-
+    
     except Exception as e:
         return f"Error analyzing video: {e}"
 
-uploaded_file = st.file_uploader("Choose a video file", type=["mp4"])
 
-if uploaded_file is not None:
-    # Display the uploaded video
-    st.video(uploaded_file)
+if username:
+    uploaded_file = st.file_uploader("Choose a video file", type=["mp4"])
 
-    # Analyze the video
-    with st.spinner("Analyzing video..."):
-        analysis_result = analyze_video(uploaded_file, uploaded_file.name)
+    if uploaded_file is not None:
+        # Display the uploaded video
+        st.video(uploaded_file)
 
-    # Display the analysis
-    st.header("Analysis Results")
-    st.write(analysis_result)
+        # Analyze the video
+        with st.spinner("Analyzing video..."):
+            analysis_result = analyze_video(uploaded_file, uploaded_file.name)
 
+        # Display the analysis
+        st.header("Analysis Results")
+        st.write(analysis_result)
